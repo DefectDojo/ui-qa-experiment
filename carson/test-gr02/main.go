4de4b79363f133f1ca6858b2edec8c5db746e353
CarsonDillenbeck <carson.dillenbeck@gmail.com>
2021-07-26 11:31:04 -0500

    Added code for TEST-GR02

--- carson/test-gr02/main.go
+++ carson/test-gr02/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	ddl "github.com/DefectDojo/ui-qa-experiment/login"
@@ -26,6 +27,52 @@ func main() {
 	p := *sess.Page
 
 	time.Sleep(time.Millisecond * 200)
+
+	// Hover over user icon
+	// #side-menu > li:nth-child(9) > a:nth-child(1)
+	p.MustElement("#side-menu > li:nth-child(9) > a:nth-child(1)").MustHover()
+
+	// Click on groups
+	// #side-menu > li:nth-child(9) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)
+	p.MustElement("#side-menu > li:nth-child(9) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)").MustClick()
+
+	// Edit correct Group
+	row := 0
+	for j := 2; j <= 15; j++ {
+		fmt.Println(j)
+		// #groups > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)
+		selector := "#groups > tbody:nth-child(1) > tr:nth-child(" + strconv.Itoa(j) + ") > td:nth-child(2)"
+		fmt.Println(selector)
+		name := p.MustElement(selector).MustText()
+		fmt.Println(name)
+		if name == "Test Group" {
+			// Matched correct username
+			fmt.Println("We matched")
+			row = j
+			j = 15
+		}
+	}
+	fmt.Println("After the loop")
+
+	// Edit correct Group
+	// #groups > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1) > b:nth-child(1)
+	p.MustElement("#groups > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1) > b:nth-child(1)").MustClick()
+
+	// Click on edit
+	// .open > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)
+	p.MustElement(".open > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)").MustClick()
+
+	// Fill out name
+	// #id_name
+	p.MustElement("#id_name").MustInput("Test Group 2")
+
+	// Fill out description
+	// .CodeMirror > div:nth-child(1) > textarea:nth-child(1)
+	p.MustElement(".CodeMirror > div:nth-child(1) > textarea:nth-child(1)").MustInput("Description for Test Group 2")
+
+	// Click on submit button
+	// input.btn
+	p.MustElement("input.btn").MustClick()
 
 	time.Sleep(time.Millisecond * 200)
 
